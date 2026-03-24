62784545d8b56b26af326530a9412d9135e03033
junindosite <juarezjunin2005@gmail.com>
2026-03-24 19:03:44 -0300

    feat: cria endpoints REST de Categoria

--- src/main/java/com/junior/crud/controller/CategoriaController.java
+++ src/main/java/com/junior/crud/controller/CategoriaController.java
@@ -1,4 +1,38 @@
 package com.junior.crud.controller;
 
+import com.junior.crud.entity.Categoria;
+import com.junior.crud.service.CategoriaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
 public class CategoriaController {
+
+    private final CategoriaService service;
+
+    public CategoriaController(CategoriaService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Categoria salvar(@RequestBody Categoria categoria) {
+        return service.salvar(categoria);
+    }
+
+    @GetMapping
+    public List<Categoria> listar() {
+        return service.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Categoria buscar(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
 }
