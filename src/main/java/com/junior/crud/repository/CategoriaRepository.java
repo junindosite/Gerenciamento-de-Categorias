59d2c7a39c6632c8effcb53a05d336d006e05017
junindosite <juarezjunin2005@gmail.com>
2026-03-24 19:02:16 -0300

    feat: implementa service de Tarefa com CRUD

--- src/main/java/com/junior/crud/repository/CategoriaRepository.java
+++ src/main/java/com/junior/crud/repository/CategoriaRepository.java
@@ -3,5 +3,5 @@ package com.junior.crud.repository;
 import com.junior.crud.entity.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoriaRepository extends JpaRepository <Categoria,Long {
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 }
--- src/main/java/com/junior/crud/service/CategoriaService.java
+++ src/main/java/com/junior/crud/service/CategoriaService.java
@@ -1,4 +1,34 @@
 package com.junior.crud.service;
 
+import com.junior.crud.entity.Categoria;
+import com.junior.crud.repository.CategoriaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoriaService {
+
+    private final CategoriaRepository repository;
+
+    public CategoriaService(CategoriaRepository repository) {
+        this.repository = repository;
+    }
+
+    public Categoria salvar(Categoria categoria) {
+        return repository.save(categoria);
+    }
+
+    public List<Categoria> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Categoria buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Categoria não encontrada"));
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
 }
